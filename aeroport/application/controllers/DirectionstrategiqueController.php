566ee0524310ae10e4749b8ca849ea251f4b5f31
kevin <kevin.danglot@wanadoo.fr>
2012-11-20 17:00:37 +0100

    Ajout du controller DirectionstrategiqueController

--- aeroport/application/controllers/DirectionstrategiqueController.php
+++ aeroport/application/controllers/DirectionstrategiqueController.php
@@ -3,20 +3,12 @@ class DirectionstrategiqueController extends Zend_Controller_Action {
 	// direction strategique
 	public function init() {}
 
-	public function indexAction() {
-		echo 'ds/index';
-		echo '<br />';
-		$auth = Zend_Auth::getInstance();
-		$identity = $auth->getIdentity();
-		echo 'Bienvenue ' . $identity->login;	
-		
+	public function indexAction() {	}
+	
+	public function ajouterligneAction() {
 		$formAjouterLigne = new Application_Form_Ajouterligne();
 		$this->view->formAjouterLigne = $formAjouterLigne;
-		
-		
 	}
-	
-	public function ajouterligneAction() {}
 	
 	public function modifierligneAction() {}
 
