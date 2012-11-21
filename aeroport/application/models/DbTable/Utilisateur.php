3773e7c62cfb139b6484302edff8860d826d84ff
kevin <kevin.danglot@wanadoo.fr>
2012-11-21 14:18:42 +0100

    Renommage du model User en Utilisateur

---
+++ aeroport/application/models/DbTable/Utilisateur.php
@@ -0,0 +1,9 @@
+<?php
+
+class Application_Model_DbTable_Utilisateur extends Zend_Db_Table_Abstract {
+	
+	// nom de la de table
+	protected $_name = 'utilisateur';
+
+} // Applicaion_Model_DbTable_User
+
