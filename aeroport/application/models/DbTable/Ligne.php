2544c69a6fe17a029d4bf09a185bbebf021b7ebb
kevin <kevin.danglot@wanadoo.fr>
2012-11-21 14:14:09 +0100

    Création du model Ligne avec 3 methodes (ajouter, modifier et supprimer)

---
+++ aeroport/application/models/DbTable/Ligne.php
@@ -0,0 +1,28 @@
+<?php
+class Application_Model_DbTable_Ligne extends Zend_Db_Table_Abstract {
+
+	protected $_name = 'ligne';
+	
+	public function ajouterLigne($heureDepart, $duree, $periodicite) {
+		$data = array(
+				'heureDepart' => $heureDepart,
+				'duree' => $duree,
+				'typePeriodicite' => $periodicite
+		);
+		$this->insert($data);
+	} // ajouterLigne()
+	
+	public function modifierLigne($id, $heureDepart, $duree, $periodicite) {
+		$data = array(
+				'heureDepart' => $heureDepart,
+				'duree' => $duree,
+				'typePeriodicite' => $periodicite
+		);
+		$this->update($data, 'id = '. (int)$id);
+	} // modifierLigne()
+	
+	public function supprimerLigne($id) {
+		$this->delete('id =' . (int)$id);
+	} // supprimerLigne()
+
+} // Applicaion_Model_DbTable_Ligne
