3ecb9150d98686f3a7b55902752b7b76432d903f
kevin <kevin.danglot@wanadoo.fr>
2012-11-21 14:16:34 +0100

    Modification du controller Directionstrategique

--- aeroport/application/controllers/DirectionstrategiqueController.php
+++ aeroport/application/controllers/DirectionstrategiqueController.php
@@ -3,13 +3,21 @@ class DirectionstrategiqueController extends Zend_Controller_Action {
 	// direction strategique
 	public function init() {}
 
-	public function indexAction() {	}
+	public function indexAction() {
+		$lignes = new Application_Model_DbTable_Ligne();
+		$this->view->lignes = $lignes->fetchAll();
+		/*$db = Zend_Registry::get('db');
+		$dbAdapter = new Zend_Auth_Adapter_DbTable($db, 'ligne', 'heure', 'motDePasse');*/
+	}
 	
 	public function ajouterligneAction() {
 		$formAjouterLigne = new Application_Form_Ajouterligne();
 		$this->view->formAjouterLigne = $formAjouterLigne;
 	}
 	
-	public function modifierligneAction() {}
+	public function modifierligneAction() {
+		$formModifierLigne = new Application_Form_Modifierligne();
+		$this->view->formModifierLigne = $formModifierLigne;
+	}
 
 }
