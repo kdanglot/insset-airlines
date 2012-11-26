45e7b4528cd1be0635b214074874cbaf890a2f78
kevin <kevin.danglot@wanadoo.fr>
2012-11-26 10:58:24 +0100

    Ajout des methodes dans la classe Ligne

--- aeroport/application/models/DbTable/Ligne.php
+++ aeroport/application/models/DbTable/Ligne.php
@@ -3,6 +3,14 @@ class Application_Model_DbTable_Ligne extends Zend_Db_Table_Abstract {
 
 	protected $_name = 'ligne';
 	
+	public function afficherLesLignes() {
+		$db = Zend_Registry::get('db');
+		$sql = 'SELECT l.idLigne, duree, typePeriodicite, heureDepart FROM ligne l, trajet t WHERE l.idLigne = t.idLigne';
+		$result = $db->fetchAll($sql);
+		
+		return $result;	
+	}
+	
 	public function ajouterLigne($heureDepart, $duree, $periodicite) {
 		$data = array(
 				'heureDepart' => $heureDepart,
