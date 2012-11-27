bedb90ab000bcd5e964f15e96f6f2c2e25d52712
kevin <kevin.danglot@wanadoo.fr>
2012-11-27 21:13:51 +0100

    Ajout requête pour afficher les lignes

--- aeroport/application/models/DbTable/Ligne.php
+++ aeroport/application/models/DbTable/Ligne.php
@@ -5,7 +5,31 @@ class Application_Model_DbTable_Ligne extends Zend_Db_Table_Abstract {
 	
 	public function afficherLesLignes() {
 		$db = Zend_Registry::get('db');
-		$sql = "SELECT LIG_id FROM lignes";		
+		$sql = "SELECT depart.LIG_id, idAeroportDepart, nomAeroportDepart, idVilleDepart, nomVilleDepart, 
+						idAeroportArrivee, nomAeroportArrivee, idVilleArrivee, nomVilleArrivee
+				FROM (
+					SELECT AER_id idAeroportArrivee, trajets.LIG_id, AER_nom nomAeroportArrivee,
+					 	VIL_nom nomVilleArrivee, VIL_id idVilleArrivee
+					FROM trajets
+					NATURAL JOIN aeroports
+					NATURAL JOIN aeroportsappartiennentvilles
+					NATURAL JOIN villes, (
+						SELECT COUNT( * ) c, LIG_id
+						FROM trajets
+						GROUP BY LIG_id
+						)nombre
+					WHERE trajets.TRA_ordre = nombre.c
+					AND trajets.LIG_id = nombre.LIG_id
+					)arrivee, (
+					SELECT AER_id idAeroportDepart, LIG_id, AER_nom nomAeroportDepart, VIL_id idVilleDepart, VIL_nom nomVilleDepart
+					FROM trajets
+					NATURAL JOIN aeroports
+					NATURAL JOIN aeroportsappartiennentvilles
+					NATURAL JOIN villes
+					WHERE TRA_ordre =  '1'
+					)depart
+					WHERE depart.LIG_id = arrivee.LIG_id";
+				
 		$result = $db->fetchAll($sql);
 		
 		return $result;	
