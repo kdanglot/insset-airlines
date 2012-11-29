c0791f3bd62e20b6974607b9965b1b1f64dd7378
dimitry-gosselin <dimitry.gosselin@gmail.com>
2012-11-29 09:39:50 +0100

    Debut du DRH controller.

--- aeroport/application/controllers/DrhController.php
+++ aeroport/application/controllers/DrhController.php
@@ -2,8 +2,6 @@
 class DrhController extends Zend_Controller_Action {
 	
 	public function indexAction() {
-		echo 'drh/index';
-		echo '<br />';
 		$auth = Zend_Auth::getInstance();
 		$identity = $auth->getIdentity();
 		echo 'Bienvenue ' . $identity->UTI_login;
