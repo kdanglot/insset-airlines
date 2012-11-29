62ed8a24750ae597d544b393da28291a89a794c2
dimitry-gosselin <dimitry.gosselin@gmail.com>
2012-11-29 16:19:58 +0100

    Requete d'affichage des pilotes.

--- aeroport/application/models/DbTable/Utilisateur.php
+++ aeroport/application/models/DbTable/Utilisateur.php
@@ -3,7 +3,9 @@
 class Application_Model_DbTable_Utilisateur extends Zend_Db_Table_Abstract {
 	
 	// nom de la de table
-	protected $_name = 'utilisateur';
+	protected $_name = 'utilisateurs';
+	protected $primary = 'UTI_id';
+    protected $_dependentTables = 'Application_Model_DbTable_Pilote';
 
 } // Applicaion_Model_DbTable_User
 
