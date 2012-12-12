3d7b195dfd29d90a3c3a802bb4a6715459fb2c6c
AntoineKaszczyc <antoine.kaszczyc@gmail.com>
2012-12-12 14:48:42 +0100

    Correction

--- aeroport/application/models/DbTable/Utilisateur.php
+++ aeroport/application/models/DbTable/Utilisateur.php
@@ -14,21 +14,6 @@ class Application_Model_DbTable_Utilisateur extends Zend_Db_Table_Abstract {
 							'refTableClass' => 'Application_Model_DbTable_TypeUtilisateur'
 					)
 			);
-
-
-	// public function getAliasTypeUtilisateur($idUtilisateur) {
-		// $utilisateur = $this->find($idUtilisateur)->current();
-		// $infos = $utilisateur->findParentApplication_Model_DbTable_TypeUtilisateur();
-		// return $infos->TUTI_alias;
-		
-// 		$db = Zend_Registry::get('db');
-// 		$sql = 'SELECT u.TUTI_id, TUTI_libelle 
-// 				FROM utilisateurs u, typesutilisateurs tu
-// 				WHERE u.TUTI_id = tu.TUTI_id
-// 				AND u.TUTI_id = '.$idUtilisateur.';';
-// 		$res = $db->fetchAll($sql);
-// 		return $res;
-	// }
 	
 	public function ajouterUtilisateur($idUtilisateur, $utilisateur) {
 		$utilisateurBDD = $this->find($idUtilisateur)->current();
