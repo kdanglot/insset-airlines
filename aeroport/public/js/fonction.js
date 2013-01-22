665f9d24340ddb056ea3150cfdcda5235e7a0114
kevin <kevin.danglot@wanadoo.fr>
2013-01-22 16:20:12 +0100

    modification de la fonction remplirSelect()

--- aeroport/public/js/fonction.js
+++ aeroport/public/js/fonction.js
@@ -1,50 +1,4 @@
 jQuery(function($){
-	// permet de remplir dynamiquement les listes déroulantes
-	// des aéroports de départs et d'arrivés en fonction du choisit
-	function remplirSelect(obj) {
-		$("#"+obj.id).click(function(){		
-			idSelect = this.id;
-			// permet de récupérer la valeur de l'option
-		    idPays = $('select#'+idSelect).val();
-		    // début de l'ajax
-		    $.ajax({
-		        type: "POST",
-		        // page qui fera le traitement
-		        url: "/ajax/aeroportbypays/idPays/"+idPays,
-		        dataType: "json",
-		        success: function(response){
-		        	// on récupère le résultat du traitement 
-		        	var optionData = response;
-		        	var obj = null;	        	
-			        var i = 1;
-			        
-		        	if (idSelect == 'pays-depart') {
-		        		obj = document.getElementById('aeroport-depart');
-		        	}
-		        	else {
-		        		obj = document.getElementById('aeroport-arrive');
-		        	}          
-		    
-		        	// on vérifie qui la réponse contient bien qqch
-		            if (optionData.length != 0 ) {
-		            	// permet de vider le select 
-		            	$('select#'+obj.id).empty();
-		            	// on rajoute l'option par défaut
-		                $('select#'+obj.id).append('<option value="-1" selected="selected">Choisissez un aéorport</option>');
-		            	
-		                // on ajoute le résultat au select
-		            	for (key in optionData) {                    	
-		                   	obj.options[i] = new Option(optionData[key].AER_nom, optionData[key].AER_id);
-		             		i++;
-		               	}               			
-		            }			
-		        }
-		    });
-		    
-		    return false;
-		
-		});
-	} // function test
 	
 	$(".terminerMaintenance").click(function(){		
 		var typeMaintenance;
@@ -79,3 +33,46 @@ jQuery(function($){
 		document.location.href= urlEnregistrementAvionDisponible+'/idAvion/'+$(this).attr("idAvion");
 	});
 });
+
+function remplirSelect(obj) {
+	$("#"+obj.id).click(function(){		
+		idSelect = this.id;
+		// permet de récupérer la valeur de l'option
+	    idPays = $('select#'+idSelect).val();
+	    // début de l'ajax
+	    $.ajax({
+	        type: "POST",
+	        // page qui fera le traitement
+	        url: "/ajax/aeroportbypays/idPays/"+idPays,
+	        dataType: "json",
+	        success: function(response){
+	        	// on récupère le résultat du traitement 
+	        	var optionData = response;
+	        	var obj = null;	        	
+		        var i = 1;
+		        
+	        	if (idSelect == 'pays-depart') {
+	        		obj = document.getElementById('aeroport-depart');
+	        	}
+	        	else {
+	        		obj = document.getElementById('aeroport-arrive');
+	        	}          
+	    
+	        	// on vérifie qui la réponse contient bien qqch
+	            if (optionData.length != 0 ) {
+	            	// permet de vider le select 
+	            	$('select#'+obj.id).empty();
+	            	// on rajoute l'option par défaut
+	                $('select#'+obj.id).append('<option value="-1" selected="selected">Choisissez un aéorport</option>');
+	            	
+	                // on ajoute le résultat au select
+	            	for (key in optionData) {                    	
+	                   	obj.options[i] = new Option(optionData[key].AER_nom, optionData[key].AER_id);
+	             		i++;
+	               	}               			
+	            }			
+	        }
+	    });		    
+	    return false;		
+	});
+} // function remplirSelect()
