c71cf858dd1e439507465f2dd13cd76b391d41ba
kevin <kevin.danglot@wanadoo.fr>
2013-01-29 17:07:48 +0100

    Ajout de la fonction getTypeIncident

--- aeroport/application/models/DbTable/TypeIncident.php
+++ aeroport/application/models/DbTable/TypeIncident.php
@@ -3,4 +3,10 @@ class Application_Model_DbTable_TypeIncident extends Zend_Db_Table_Abstract {
 	protected $_name = 'typesIncident';
 	protected $primary = 'TINC_id';
 	
+	public function getTypeIncident() {
+		$db = Zend_Registry::get('db');
+		$sql = 'SELECT * FROM typesincident;';
+		$result = $db->fetchAll($sql);		
+		return $result;
+	}
 }
