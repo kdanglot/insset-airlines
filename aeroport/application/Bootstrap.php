44ad0b26c9130bbd98e3b597cc78bc718f61d984
AntoineKaszczyc <antoine.kaszczyc@gmail.com>
2013-01-29 17:12:57 +0100

    REfonte AUth

--- aeroport/application/Bootstrap.php
+++ aeroport/application/Bootstrap.php
@@ -15,6 +15,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 	
 	protected function _initSession() {
 		$session = new Zend_Session_Namespace('projetAeroport', true);
+		
 		return $session;
 		
 	}
@@ -27,6 +28,62 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 		
 	} 
 	
+	protected function _initAcl(){
+		
+		$acl = new Zend_Acl();
+		
+		$acl->addResource(new Zend_Acl_Resource('administrateur'));
+		$acl->addResource(new Zend_Acl_Resource('directionstrategique'));
+		$acl->addResource(new Zend_Acl_Resource('drh'));
+		$acl->addResource(new Zend_Acl_Resource('ajax'));
+		$acl->addResource(new Zend_Acl_Resource('error'));
+		$acl->addResource(new Zend_Acl_Resource('exploitation'));
+		$acl->addResource(new Zend_Acl_Resource('index'));
+		$acl->addResource(new Zend_Acl_Resource('maintenance'));
+		$acl->addResource(new Zend_Acl_Resource('planning'));
+		
+		$acl->addRole(new Zend_Acl_Role('invite'));
+		$acl->addRole(new Zend_Acl_Role('drh'), 'invite');
+		$acl->addRole(new Zend_Acl_Role('directionstrategique'), 'invite');
+		$acl->addRole(new Zend_Acl_Role('maintenance'), 'invite');
+		$acl->addRole(new Zend_Acl_Role('pilote'), 'invite');
+		$acl->addRole(new Zend_Acl_Role('planning'), 'invite');
+		$acl->addRole(new Zend_Acl_Role('exploitation'), 'invite');
+		$acl->addRole(new Zend_Acl_Role('administrateur'), array('drh', 'directionstrategique', 'maintenance', 'pilote', 'planning', 'exploitation'));
+		
+		$acl->deny(null, null);
+		
+		$acl->allow('invite', 'index');
+		$acl->allow('invite', 'error');
+		$acl->allow('invite', 'ajax');
+		
+		$acl->allow('directionstrategique', 'directionstrategique');
+		$acl->allow('drh', 'drh');
+		$acl->allow('maintenance', 'maintenance');
+		$acl->allow('planning', 'planning');
+		$acl->allow('exploitation', 'exploitation');
+		
+		$acl->allow('administrateur', 'administrateur');
+		
+		Zend_Registry::set('acl', $acl);
+		
+		$session = new Zend_Session_Namespace('role');
+		
+		$auth = Zend_Auth::getInstance();
+			
+		if($auth->hasIdentity()){
+		
+			$identity = $auth->getIdentity();
+			$session->role = $identity->TUTI_alias;
+			
+		}
+		else{
+			$session->role = 'invite';
+		}
+		
+		
+		// var_dump($session->role);exit;
+	}
 	
 } // Bootstrap
 
