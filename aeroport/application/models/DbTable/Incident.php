d8f1c78e20a909c3cf839320114f9a28d99ea7df
kevin <kevin.danglot@wanadoo.fr>
2013-01-29 17:08:36 +0100

    Ajout de la fonction insertIncident

--- aeroport/application/models/DbTable/Incident.php
+++ aeroport/application/models/DbTable/Incident.php
@@ -2,5 +2,8 @@
 class Application_Model_DbTable_Incident extends Zend_Db_Table_Abstract {
 	protected $_name = 'incidents';
 	protected $primary = 'INC_id';
-	
+		
+	public function insertIncident(array $data) {
+		return $this->insert($data);
+	}
 }
