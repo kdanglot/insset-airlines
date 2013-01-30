f46b4fd17adba6fca3abd3af0f2b1325eebb0fb4
kevin <kevin.danglot@wanadoo.fr>
2013-01-30 14:38:14 +0100

    ajout de la fonction getIncident

--- aeroport/application/models/DbTable/Incident.php
+++ aeroport/application/models/DbTable/Incident.php
@@ -2,7 +2,10 @@
 class Application_Model_DbTable_Incident extends Zend_Db_Table_Abstract {
 	protected $_name = 'incidents';
 	protected $primary = 'INC_id';
-		
+
+	public function getIncidents() {
+		return $this->fetchAll();
+	}
 	public function insertIncident(array $data) {
 		return $this->insert($data);
 	}
