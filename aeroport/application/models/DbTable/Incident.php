9e3d29f3260ea5af7c3345de78757069dab87e21
kevin <kevin.danglot@wanadoo.fr>
2013-01-30 22:59:08 +0100

    Création du model incident avec les fonctions getDbAdapter(), getIncidents(), getIncidentByIdVol() et insertIncident

--- aeroport/application/models/DbTable/Incident.php
+++ aeroport/application/models/DbTable/Incident.php
@@ -3,9 +3,26 @@ class Application_Model_DbTable_Incident extends Zend_Db_Table_Abstract {
 	protected $_name = 'incidents';
 	protected $primary = 'INC_id';
 
+	// permet de récupère l'adapter
+	public function getDbAdapter() {
+		return Zend_Registry::get('db');
+	}
+	
+	// permet de récupère tous les incidents
 	public function getIncidents() {
 		return $this->fetchAll();
 	}
+	
+	// permet de récupère tous les incidents d'un vol
+	public function getIncidentByIdVol($idVol) {
+		$sql = 'SELECT * 
+				FROM incidents i, typesincident ti 
+				WHERE VOL_id ='.$idVol.'
+				AND i.TINC_id = ti.TINC_id';
+		return $this->getDbAdapter()->fetchAll($sql);
+	}
+	
+	// permet d'ajouter un incident
 	public function insertIncident(array $data) {
 		return $this->insert($data);
 	}
